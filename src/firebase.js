27069b3b21255946509066c211a9ceb61ba4d2ea
karlasotelophotography-ctrl <karla.sotelo.photography@hotmail.com>
2026-05-18 17:02:15 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,22 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, GoogleAuthProvider, browserLocalPersistence, setPersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey:            "AIzaSyAlb1b5fcF_BoP9IDhEm-1C6AidcpZRp4s",
-  authDomain:        "kit-finanzas-karla.firebaseapp.com",
+  authDomain:        "kit-finanzas.vercel.app",
   projectId:         "kit-finanzas-karla",
   storageBucket:     "kit-finanzas-karla.firebasestorage.app",
   messagingSenderId: "171814775539",
   appId:             "1:171814775539:web:0b78ddea1ac59cef1e2586"
 }
 
-const app = initializeApp(firebaseConfig)
+const app  = initializeApp(firebaseConfig)
+const auth = getAuth(app)
 
-export const auth     = getAuth(app)
+// Persistencia local para que la sesión se mantenga en PWA
+setPersistence(auth, browserLocalPersistence).catch(() => {})
+
+export { auth }
 export const db       = getFirestore(app)
 export const provider = new GoogleAuthProvider()
